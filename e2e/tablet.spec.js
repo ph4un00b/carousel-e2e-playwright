c803079a570d1e9c6a880dfeb1e38b58685ed3a8
ph4u <faunotv@gmail.com>
2022-03-01 15:19:24 -0600

    feat(Root): Add Tablet / Desktop Structure.

--- e2e/tablet.spec.js
+++ e2e/tablet.spec.js
@@ -1,23 +1,22 @@
 // @no-check
 const { test, expect, devices } = require("@playwright/test");
 
-test.use({
-  ...devices['iPhone 11'],
-});
+    test.use({
+      ...devices['iPad Mini landscape'],
+    });
 
-test.describe("Mobile Carousel", () => {
-
+test.describe("Tablet Carousel", () => {
   test("markup should be visible.", async ({ page }) => {
     await page.goto("http://localhost:3000/");
     await expect(page.locator("#carousel")).toBeVisible();
     await expect(page.locator("text=<")).toBeVisible();
     await expect(page.locator(".carousel-image")).toBeVisible();
     await expect(page.locator("text=>")).toBeVisible();
+    await expect(page.locator("text=Featured Boxes")).toBeVisible();
     await expect(page.locator(".carousel-title")).toBeVisible();
     await expect(page.locator(".carousel-description")).toBeVisible();
     await expect(page.locator("text=Learn how it works")).toBeVisible();
     await expect(page.locator("text=or View available boxes")).toBeVisible();
-    await expect(page.locator("text=Featured Boxes")).toBeHidden();
   });
 });
 
