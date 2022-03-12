53cec67127620922f882b0b09f76c13b25cfe1cb
ph4u <faunotv@gmail.com>
2022-03-11 19:37:30 -0600

    perf(Root): Minity CSS.
    We use PostCSS in order to merge css files.
    
    We use ParcelCSS in order to minify the files proccessed
    
    by lume.

--- _config.js
+++ _config.js
@@ -1,5 +1,7 @@
 import lume from "lume/mod.ts";
 import imagick from "https://raw.githubusercontent.com/lumeland/experimental-plugins/main/imagick/imagick.ts";
+import postcss from "lume/plugins/postcss.ts";
+import parcelcss, { version } from "lume/plugins/parcel_css.ts";
 
 const nunjucks = {
   extensions: [".html"],
@@ -15,11 +17,35 @@ site
   .ignore("playwright-report")
   .ignore("regression")
   .use(imagick())
+  .use(postcss())
+
 
 site.copy("main.js");
-site.copy("styles.css");
 site.copy("static/controllers");
-site.copy("static/assets");
 site.copy("dynamic/controllers");
-site.copy("dynamic/assets");
+site.copy("dynamic/assets/models.json");
+
+
+site.use(parcelcss({
+  extensions: [".css"],
+  sourceMap: false,
+  options: {
+    minify: true,
+    drafts: {
+      nesting: true,
+      customMedia: true,
+    },
+    targets: {
+      android: version(98),
+      chrome: version(98),
+      edge: version(98),
+      firefox: version(97),
+      ios_saf: version(12),
+      safari: version(15),
+      opera: version(83),
+      samsung: version(16),
+    },
+  },
+}));
+
 export default site;
