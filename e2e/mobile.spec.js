f499c1c4347c640069107bb9abe6484028a6c30f
ph4u <faunotv@gmail.com>
2022-03-13 17:22:33 -0600

    refactor(Root): tests.

--- e2e/mobile.spec.js
+++ e2e/mobile.spec.js
@@ -1,5 +1,10 @@
 // @no-check
 const { test, expect, devices } = require("@playwright/test");
+const {
+  expect_common_markup,
+  expect_static_controls,
+  expect_dynamic_controls,
+} = require("./shared");
 
 test.use({
   ...devices["iPhone 11"],
@@ -11,15 +16,7 @@ test.describe("Mobile Carousel", () => {
       waitUntil: "networkidle",
     });
 
-    await expect(page.locator("html")).toHaveClass(/webp/);
-    await expect(page.locator("#carousel")).toBeVisible();
-    await expect(page.locator("text=<")).toBeVisible();
-    await expect(page.locator(".carousel-image")).toBeVisible();
-    await expect(page.locator("text=>")).toBeVisible();
-    await expect(page.locator(".item-name")).toBeVisible();
-    await expect(page.locator(".item-description")).toBeVisible();
-    await expect(page.locator("text=Learn how it works")).toBeVisible();
-    await expect(page.locator("text=or View available boxes")).toBeVisible();
+    await expect_common_markup(page);
     await expect(page.locator("text=Featured Boxes")).toBeHidden();
   });
 
@@ -28,159 +25,23 @@ test.describe("Mobile Carousel", () => {
       waitUntil: "networkidle",
     });
 
-    await expect(page.locator("html")).toHaveClass(/webp/);
-    await expect(page.locator("#carousel")).toBeVisible();
-    await expect(page.locator("text=<")).toBeVisible();
-    await expect(page.locator(".carousel-image")).toBeVisible();
-    await expect(page.locator("text=>")).toBeVisible();
-    await expect(page.locator(".item-name")).toBeVisible();
-    await expect(page.locator(".item-description")).toBeVisible();
-    await expect(page.locator("text=Learn how it works")).toBeVisible();
-    await expect(page.locator("text=or View available boxes")).toBeVisible();
+    await expect_common_markup(page);
     await expect(page.locator("text=Featured Boxes")).toBeHidden();
   });
 
   test("static controls", async ({ page }) => {
-    // Go to http://localhost:3000/static/
     await page.goto("http://localhost:3000/static/", {
       waitUntil: "networkidle",
     });
-    // Click text=Alchemy
-    await expect(page.locator("text=Alchemy")).toBeVisible();
-    // Click text=Stock your home bar with pro-level cocktail wares.
-    await expect(
-      page.locator("text=Stock your home bar with pro-level cocktail wares."),
-    ).toBeVisible();
-    // Click [aria-label="product\:\ Alchemy"] >> text=>
-    await page.locator("#next-0").click();
-    // Click h1:has-text("Aged")
-    await expect(page.locator('h1:has-text("Aged")')).toBeVisible();
-    // Click text=A toast to the perfectly aged cocktail.
-    await expect(page.locator("text=A toast to the perfectly aged cocktail."))
-      .toBeVisible();
-    // Click [aria-label="product\:\ Aged"] >> text=>
-    await page.locator("#next-1").click();
-    // Click text=Refresh
-    await expect(page.locator("text=Refresh")).toBeVisible();
-    // Click text=Bring some class to your morning routine.
-    await expect(page.locator("text=Bring some class to your morning routine."))
-      .toBeVisible();
-    // Click [aria-label="product\:\ Refresh"] >> text=>
-    await page.locator("#next-2").click();
-    // Click text=Weekender
-    await expect(page.locator("text=Weekender")).toBeVisible();
-    // Click text=Don't just get away from the crowds — stand out from them.
-    await expect(
-      page.locator(
-        "text=Don't just get away from the crowds — stand out from them.",
-      ),
-    ).toBeVisible();
-    // Click [aria-label="product\:\ Weekender"] >> text=>
-    await page.locator("#next-3").click();
-    // Click text=Alchemy
-    await expect(page.locator("text=Alchemy")).toBeVisible();
-    // Click text=Stock your home bar with pro-level cocktail wares.
-    await expect(
-      page.locator("text=Stock your home bar with pro-level cocktail wares."),
-    ).toBeVisible();
-    // Click [aria-label="product\:\ Alchemy"] >> text=<
-    await page.locator("#back-0").click();
-    await expect(
-      page.locator(
-        "text=Don't just get away from the crowds — stand out from them.",
-      ),
-    ).toBeVisible();
-    // Click [aria-label="product\:\ Weekender"] >> text=<
-    await page.locator("#back-3").click();
-    await expect(page.locator("text=Bring some class to your morning routine."))
-      .toBeVisible();
-    // Click [aria-label="product\:\ Refresh"] >> text=<
-    await page.locator("#back-2").click();
-    await expect(page.locator("text=A toast to the perfectly aged cocktail."))
-      .toBeVisible();
-    // Click [aria-label="product\:\ Aged"] >> text=<
-    await page.locator("#back-1").click();
-    await expect(
-      page.locator("text=Stock your home bar with pro-level cocktail wares."),
-    ).toBeVisible();
-    // Click [aria-label="product\:\ Alchemy"] >> text=<
-    await page.locator("#back-0").click();
-    await expect(page.locator("text=A toast to the perfectly aged cocktail."))
-      .toBeVisible();
-    // Click [aria-label="product\:\ Aged"] >> text=<
-    await expect(page.locator('[aria-label="product\\:\\ Aged"] >> text=<'))
-      .toBeVisible();
+
+    await expect_static_controls(page);
   });
 
   test("dynamic controls", async ({ page }) => {
-    // Go to http://localhost:3000/static/
     await page.goto("http://localhost:3000/dynamic/", {
       waitUntil: "networkidle",
     });
-    // Click text=Alchemy
-    await expect(page.locator("text=Alchemy")).toBeVisible();
-    // Click text=Stock your home bar with pro-level cocktail wares.
-    await expect(
-      page.locator("text=Stock your home bar with pro-level cocktail wares."),
-    ).toBeVisible();
-    // Click [aria-label="product\:\ Alchemy"] >> text=>
-    await page.locator("#next").click();
-    // Click h1:has-text("Aged")
-    await expect(page.locator('h1:has-text("Aged")')).toBeVisible();
-    // Click text=A toast to the perfectly aged cocktail.
-    await expect(page.locator("text=A toast to the perfectly aged cocktail."))
-      .toBeVisible();
-    // Click [aria-label="product\:\ Aged"] >> text=>
-    await page.locator("#next").click();
-    // Click text=Refresh
-    await expect(page.locator("text=Refresh")).toBeVisible();
-    // Click text=Bring some class to your morning routine.
-    await expect(page.locator("text=Bring some class to your morning routine."))
-      .toBeVisible();
-    // Click [aria-label="product\:\ Refresh"] >> text=>
-    await page.locator("#next").click();
-    // Click text=Weekender
-    await expect(page.locator("text=Weekender")).toBeVisible();
-    // Click text=Don't just get away from the crowds — stand out from them.
-    await expect(
-      page.locator(
-        "text=Don't just get away from the crowds — stand out from them.",
-      ),
-    ).toBeVisible();
-    // Click [aria-label="product\:\ Weekender"] >> text=>
-    await page.locator("#next").click();
-    // Click text=Alchemy
-    await expect(page.locator("text=Alchemy")).toBeVisible();
-    // Click text=Stock your home bar with pro-level cocktail wares.
-    await expect(
-      page.locator("text=Stock your home bar with pro-level cocktail wares."),
-    ).toBeVisible();
-    // Click [aria-label="product\:\ Alchemy"] >> text=<
-    await page.locator("#back").click();
-    await expect(
-      page.locator(
-        "text=Don't just get away from the crowds — stand out from them.",
-      ),
-    ).toBeVisible();
-    // Click [aria-label="product\:\ Weekender"] >> text=<
-    await page.locator("#back").click();
-    await expect(page.locator("text=Bring some class to your morning routine."))
-      .toBeVisible();
-    // Click [aria-label="product\:\ Refresh"] >> text=<
-    await page.locator("#back").click();
-    await expect(page.locator("text=A toast to the perfectly aged cocktail."))
-      .toBeVisible();
-    // Click [aria-label="product\:\ Aged"] >> text=<
-    await page.locator("#back").click();
-    await expect(
-      page.locator("text=Stock your home bar with pro-level cocktail wares."),
-    ).toBeVisible();
-    // Click [aria-label="product\:\ Alchemy"] >> text=<
-    await page.locator("#back").click();
-    await expect(page.locator("text=A toast to the perfectly aged cocktail."))
-      .toBeVisible();
-    // Click [aria-label="product\:\ Aged"] >> text=<
-    await expect(page.locator('[aria-label="product\\:\\ Aged"] >> text=<'))
-      .toBeVisible();
+
+    await expect_dynamic_controls(page);
   });
 });
--- e2e/tablet.spec.js
+++ e2e/tablet.spec.js
@@ -1,6 +1,12 @@
 // @no-check
 const { test, expect, devices } = require("@playwright/test");
-
+const {
+  expect_common_markup,
+  expect_static_controls,
+  expect_dynamic_controls,
+} = require("./shared");
+`
+`;
 test.use({
   ...devices["iPhone 11 landscape"],
 });
@@ -11,16 +17,8 @@ test.describe("Tablet Carousel", () => {
       waitUntil: "networkidle",
     });
 
-    await expect(page.locator("html")).toHaveClass(/webp/);
-    await expect(page.locator("#carousel")).toBeVisible();
-    await expect(page.locator("text=<")).toBeVisible();
-    await expect(page.locator(".carousel-image")).toBeVisible();
-    await expect(page.locator("text=>")).toBeVisible();
+    await expect_common_markup(page);
     await expect(page.locator("text=Featured Boxes")).toBeVisible();
-    await expect(page.locator(".item-name")).toBeVisible();
-    await expect(page.locator(".item-description")).toBeVisible();
-    await expect(page.locator("text=Learn how it works")).toBeVisible();
-    await expect(page.locator("text=or View available boxes")).toBeVisible();
   });
 
   test("dynamic markup should be visible.", async ({ page }) => {
@@ -28,159 +26,23 @@ test.describe("Tablet Carousel", () => {
       waitUntil: "networkidle",
     });
 
-    await expect(page.locator("html")).toHaveClass(/webp/);
-    await expect(page.locator("#carousel")).toBeVisible();
-    await expect(page.locator("text=<")).toBeVisible();
-    await expect(page.locator(".carousel-image")).toBeVisible();
-    await expect(page.locator("text=>")).toBeVisible();
+    await expect_common_markup(page);
     await expect(page.locator("text=Featured Boxes")).toBeVisible();
-    await expect(page.locator(".item-name")).toBeVisible();
-    await expect(page.locator(".item-description")).toBeVisible();
-    await expect(page.locator("text=Learn how it works")).toBeVisible();
-    await expect(page.locator("text=or View available boxes")).toBeVisible();
   });
 
   test("static controls", async ({ page }) => {
-    // Go to http://localhost:3000/static/
     await page.goto("http://localhost:3000/static/", {
       waitUntil: "networkidle",
     });
-    // Click text=Alchemy
-    await expect(page.locator("text=Alchemy")).toBeVisible();
-    // Click text=Stock your home bar with pro-level cocktail wares.
-    await expect(
-      page.locator("text=Stock your home bar with pro-level cocktail wares."),
-    ).toBeVisible();
-    // Click [aria-label="product\:\ Alchemy"] >> text=>
-    await page.locator("#next-0").click();
-    // Click h1:has-text("Aged")
-    await expect(page.locator('h1:has-text("Aged")')).toBeVisible();
-    // Click text=A toast to the perfectly aged cocktail.
-    await expect(page.locator("text=A toast to the perfectly aged cocktail."))
-      .toBeVisible();
-    // Click [aria-label="product\:\ Aged"] >> text=>
-    await page.locator("#next-1").click();
-    // Click text=Refresh
-    await expect(page.locator("text=Refresh")).toBeVisible();
-    // Click text=Bring some class to your morning routine.
-    await expect(page.locator("text=Bring some class to your morning routine."))
-      .toBeVisible();
-    // Click [aria-label="product\:\ Refresh"] >> text=>
-    await page.locator("#next-2").click();
-    // Click text=Weekender
-    await expect(page.locator("text=Weekender")).toBeVisible();
-    // Click text=Don't just get away from the crowds — stand out from them.
-    await expect(
-      page.locator(
-        "text=Don't just get away from the crowds — stand out from them.",
-      ),
-    ).toBeVisible();
-    // Click [aria-label="product\:\ Weekender"] >> text=>
-    await page.locator("#next-3").click();
-    // Click text=Alchemy
-    await expect(page.locator("text=Alchemy")).toBeVisible();
-    // Click text=Stock your home bar with pro-level cocktail wares.
-    await expect(
-      page.locator("text=Stock your home bar with pro-level cocktail wares."),
-    ).toBeVisible();
-    // Click [aria-label="product\:\ Alchemy"] >> text=<
-    await page.locator("#back-0").click();
-    await expect(
-      page.locator(
-        "text=Don't just get away from the crowds — stand out from them.",
-      ),
-    ).toBeVisible();
-    // Click [aria-label="product\:\ Weekender"] >> text=<
-    await page.locator("#back-3").click();
-    await expect(page.locator("text=Bring some class to your morning routine."))
-      .toBeVisible();
-    // Click [aria-label="product\:\ Refresh"] >> text=<
-    await page.locator("#back-2").click();
-    await expect(page.locator("text=A toast to the perfectly aged cocktail."))
-      .toBeVisible();
-    // Click [aria-label="product\:\ Aged"] >> text=<
-    await page.locator("#back-1").click();
-    await expect(
-      page.locator("text=Stock your home bar with pro-level cocktail wares."),
-    ).toBeVisible();
-    // Click [aria-label="product\:\ Alchemy"] >> text=<
-    await page.locator("#back-0").click();
-    await expect(page.locator("text=A toast to the perfectly aged cocktail."))
-      .toBeVisible();
-    // Click [aria-label="product\:\ Aged"] >> text=<
-    await expect(page.locator('[aria-label="product\\:\\ Aged"] >> text=<'))
-      .toBeVisible();
+
+    await expect_static_controls(page);
   });
 
   test("dynamic controls", async ({ page }) => {
-    // Go to http://localhost:3000/static/
     await page.goto("http://localhost:3000/dynamic/", {
       waitUntil: "networkidle",
     });
-    // Click text=Alchemy
-    await expect(page.locator("text=Alchemy")).toBeVisible();
-    // Click text=Stock your home bar with pro-level cocktail wares.
-    await expect(
-      page.locator("text=Stock your home bar with pro-level cocktail wares."),
-    ).toBeVisible();
-    // Click [aria-label="product\:\ Alchemy"] >> text=>
-    await page.locator("#next").click();
-    // Click h1:has-text("Aged")
-    await expect(page.locator('h1:has-text("Aged")')).toBeVisible();
-    // Click text=A toast to the perfectly aged cocktail.
-    await expect(page.locator("text=A toast to the perfectly aged cocktail."))
-      .toBeVisible();
-    // Click [aria-label="product\:\ Aged"] >> text=>
-    await page.locator("#next").click();
-    // Click text=Refresh
-    await expect(page.locator("text=Refresh")).toBeVisible();
-    // Click text=Bring some class to your morning routine.
-    await expect(page.locator("text=Bring some class to your morning routine."))
-      .toBeVisible();
-    // Click [aria-label="product\:\ Refresh"] >> text=>
-    await page.locator("#next").click();
-    // Click text=Weekender
-    await expect(page.locator("text=Weekender")).toBeVisible();
-    // Click text=Don't just get away from the crowds — stand out from them.
-    await expect(
-      page.locator(
-        "text=Don't just get away from the crowds — stand out from them.",
-      ),
-    ).toBeVisible();
-    // Click [aria-label="product\:\ Weekender"] >> text=>
-    await page.locator("#next").click();
-    // Click text=Alchemy
-    await expect(page.locator("text=Alchemy")).toBeVisible();
-    // Click text=Stock your home bar with pro-level cocktail wares.
-    await expect(
-      page.locator("text=Stock your home bar with pro-level cocktail wares."),
-    ).toBeVisible();
-    // Click [aria-label="product\:\ Alchemy"] >> text=<
-    await page.locator("#back").click();
-    await expect(
-      page.locator(
-        "text=Don't just get away from the crowds — stand out from them.",
-      ),
-    ).toBeVisible();
-    // Click [aria-label="product\:\ Weekender"] >> text=<
-    await page.locator("#back").click();
-    await expect(page.locator("text=Bring some class to your morning routine."))
-      .toBeVisible();
-    // Click [aria-label="product\:\ Refresh"] >> text=<
-    await page.locator("#back").click();
-    await expect(page.locator("text=A toast to the perfectly aged cocktail."))
-      .toBeVisible();
-    // Click [aria-label="product\:\ Aged"] >> text=<
-    await page.locator("#back").click();
-    await expect(
-      page.locator("text=Stock your home bar with pro-level cocktail wares."),
-    ).toBeVisible();
-    // Click [aria-label="product\:\ Alchemy"] >> text=<
-    await page.locator("#back").click();
-    await expect(page.locator("text=A toast to the perfectly aged cocktail."))
-      .toBeVisible();
-    // Click [aria-label="product\:\ Aged"] >> text=<
-    await expect(page.locator('[aria-label="product\\:\\ Aged"] >> text=<'))
-      .toBeVisible();
+
+    await expect_dynamic_controls(page);
   });
 });
